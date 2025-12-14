d4b65e43afde5c2bc340c6648cea4713b5e694aa
Dhruv942 <dhruvpatel242231@gmail.com>
2025-12-14 20:58:50 +0530

    fix:cors issue

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, { Application, Request, Response } from "express";
+import cors from "cors";
 import mongoose from "mongoose";
 import { connectDB } from "./config/database";
 
@@ -6,6 +7,15 @@ import authRoutes from "./routes/auth.routes";
 import sweetRoutes from "./routes/sweet.routes";
 const app: Application = express();
 const PORT = Number(process.env.PORT) || 3000;
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
